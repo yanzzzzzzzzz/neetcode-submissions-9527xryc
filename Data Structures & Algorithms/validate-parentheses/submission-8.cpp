94a06107a1ecafdd16efb0c7b31500d95d0ac7e4
YenTsai <king840110@gmail.com>
2026-08-27 12:25:16 +0800

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> pairs = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+        for(char ch:s){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else if(ch == ')' || ch == ']' || ch == '}'){
+                if(st.empty() || st.top() != pairs[ch]){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
